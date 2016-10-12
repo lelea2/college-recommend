99bc36f82cb70daebb1454c80cc38f58886fdc20
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-12 12:33:34 -0700

    Initial check in

--- controllers/helpers/dispatcher.js
+++ controllers/helpers/dispatcher.js
@@ -4,11 +4,19 @@
 
 'use strict';
 
+//Range of SAT score, every  data of 100 we substract/add 8% of chance of getting in
 function getSATScore() {
 
 }
 
+//Range of SAT score, every  data of 100 we substract/add 5% of chance of getting in
 function getACTScore() {
+
+}
+
+//Range of income
+//Howhouse income need to be at least 2.5 more than tution, if less than the chance to calculate chance to get financial aids
+function getHouseHoldIncome() {
 
 }
 
