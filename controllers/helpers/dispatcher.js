b146a3d8d8b1ae23e37fcae625bce69afe226308
lelea2 <daomaikhanh1988@yahoo.com>
2016-10-15 09:04:37 -0700

    Fixing range

--- controllers/helpers/dispatcher.js
+++ controllers/helpers/dispatcher.js
@@ -9,23 +9,23 @@ function dispatch(sat_score, act_score, public_service_hour) {
   //Generate 3 algorithm based on public service_hour, expect_payment
   if (public_service_hour === 4) { //public service hour option
     return {
-      sat_score: [sat_score, sat_score + 300], //max sat score
-      act_score: [act_score, act_score + 10]
+      sat_score: [sat_score - 300, sat_score + 300], //max sat score
+      act_score: [act_score - 10, act_score + 10]
     };
   } else if (public_service_hour === 3) {
     return {
-      sat_score: [sat_score, sat_score + 200], //max sat score
-      act_score: [act_score, act_score + 8]
+      sat_score: [sat_score - 300, sat_score + 200], //max sat score
+      act_score: [act_score - 10, act_score + 8]
     };
   } else if (public_service_hour === 2) { //< 100
     return {
-      sat_score: [sat_score, sat_score + 100], //max sat score
-      act_score: [act_score, act_score + 4]
+      sat_score: [sat_score - 300, sat_score + 100], //max sat score
+      act_score: [act_score - 10, act_score + 4]
     };
   } else { // no public service
     return {
-      sat_score: [sat_score, sat_score + 50], //max sat score
-      act_score: [act_score, act_score + 2]
+      sat_score: [sat_score - 300, sat_score + 50], //max sat score
+      act_score: [act_score - 10, act_score + 2]
     };
   }
 }
